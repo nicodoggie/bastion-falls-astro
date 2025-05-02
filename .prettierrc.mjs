01239299020aeb5e4061c6a0b78c860522e12bc0
ensu <enrico.suarez@xsplit.com>
2025-05-03 04:19:12 +0800

    chore: update packaging files

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,13 +1,11 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: [
-    "prettier-plugin-astro",
-    "prettier-plugin-multiline-arrays",
-  ],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-multiline-arrays"],
   printWidth: 80,
   proseWrap: "always",
   trailingComma: "all",
   semi: true,
   multilineArraysWrapThreshold: 1,
-  
+  singleQuote: true,
+  tabWidth: 2,
 };
--- tailwind.config.mjs
+++
@@ -1,6 +0,0 @@
-import { AstroBlogTailwindPaths } from "astro-blog";
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}", ...AstroBlogTailwindPaths],
-};
